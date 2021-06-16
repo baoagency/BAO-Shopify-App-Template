6949209141a1d6ca9aefa0f22e0c6d12115dde76
Dan Gamble <dan@bao.agency>
2021-06-16 11:12:20 +0100

    Implement better host token fetching

--- app/controllers/splash_page_controller.rb
+++ app/controllers/splash_page_controller.rb
@@ -3,12 +3,13 @@ class SplashPageController < ApplicationController
   include ShopifyApp::RequireKnownShop
   include ShopifyApp::ShopAccessScopesVerification
 
-  rescue_from ActiveResource::UnauthorizedAccess do
-    redirect_to(shop_login)
-  end
+  # rescue_from ActiveResource::UnauthorizedAccess do
+  #   redirect_to(shop_login)
+  # end
 
   def index
     @host = host
+    shopify_host_cookie.set(host) if shopify_host_cookie.empty?
     @shop_origin = current_shopify_domain
 
     # required to trigger OAuth token check
@@ -19,10 +20,17 @@ class SplashPageController < ApplicationController
   end
 
   def host
+    return shopify_host_cookie.get if shopify_host_cookie.exists?
     return params[:host] if params.has_key? :host
 
     uri = URI.parse(params[:return_to])
 
     Rack::Utils.parse_nested_query(uri.query)['host']
   end
+
+  private
+
+    def shopify_host_cookie
+      @shopify_host_cookie ||= ShopifyHostCookie.new(cookies)
+    end
 end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root :to => 'splash_page#index'
-
-  get '/home', :to => 'pages#index', as: :home
-  get '/about', :to => 'pages#about', as: :about
-
-  mount ShopifyApp::Engine, at: '/'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
